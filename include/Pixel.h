62bbd8c18e324518be5156a581bd28bf2096f9c7
Dehan Wijesinghe <dehansenuth7@gmail.com>
2026-07-27 20:40:20 +0530

    first commit

---
+++ include/Pixel.h
@@ -0,0 +1,19 @@
+#ifndef PIXEL_H
+#define PIXEL_H
+
+namespace img {
+
+
+structPixel {
+        unsigned char r; //1 byte allocated for r
+        unsigned char g;
+        unsigned char b;
+        
+    };
+    
+
+}
+
+#endif
+
+
