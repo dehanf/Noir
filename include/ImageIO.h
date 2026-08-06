5d092db31541223a984f963ed44213cd5f8bad12
Dehan Wijesinghe <dehansenuth7@gmail.com>
2026-08-06 18:38:24 +0530

    header files

---
+++ include/ImageIO.h
@@ -0,0 +1,17 @@
+#ifndef IMAGE_IO_H
+#define IMAGE_IO_H
+
+#include <string>
+
+#include "Image.h"
+
+namespace img {
+
+bool savePPM(
+    const Image& image, //DO NOT COPY: pass by rerference cause real images could contain million pixels
+    const std::string& filename
+);
+
+}
+
+#endif
