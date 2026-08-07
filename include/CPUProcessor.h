4d4312ddf4520272325cb6b7491c1a4d9aa94501
Dehan Wijesinghe <dehansenuth7@gmail.com>
2026-08-07 10:53:57 +0530

    processor header file

---
+++ include/CPUProcessor.h
@@ -0,0 +1,28 @@
+#ifndef CPU_PROCESSOR_H
+#define CPU_PROCESSOR_H
+
+#include "Image.h"
+
+namespace img{
+
+class CPUPRocessor{
+
+public:
+    static void grayscale(Image& image);
+    static void invert(Image& image);
+
+    static void adjustBrightness(Image& image, int brightness);
+
+    static void adjustContrast(Image& image, float contrast);
+
+    static void threshold(Image& image, unsigned char threshold=128);
+
+private:
+
+    static unsigned char clamp(int value); // clamping stays private because it is a helper function that is only used internally by the class methods
+    // double uses 8 bytes, but unsigned char uses only 1 byte
+};
+}
+
+
+#endif CPU_PROCESSOR_H
