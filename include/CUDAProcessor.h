392508018b8681c50e017a68c16fed8c24a7c85e
dehanf <dehansenuth7@gmail.com>
2026-08-13 03:39:34 +0530

    header file for cuda processor

---
+++ include/CUDAProcessor.h
@@ -0,0 +1,17 @@
+#ifndef CUDA_PROCESSOR_H
+#define CUDA_PROCESSOR_H
+
+#include "Image.h"
+
+namespace img {
+
+class CUDAProcessor {
+public:
+    static bool invert(Image& image);
+};
+
+}
+
+#endif
+
+
