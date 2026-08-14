ccfaf303df12235c4d1807e0d2f6e26bc61e5142
Dehan Wijesinghe <dehansenuth7@gmail.com>
2026-08-14 19:49:31 +0530

    header files including all the operation funtions

--- include/CUDAProcessor.h
+++ include/CUDAProcessor.h
@@ -8,7 +8,23 @@ namespace img {
 class CUDAProcessor {
 public:
     static bool invert(Image& image);
+
     static bool grayscale(Image& image);
+
+    static bool brightness(
+        Image& image,
+        int amount
+    );
+
+    static bool contrast(
+        Image& image,
+        float factor
+    );
+
+    static bool threshold(
+        Image& image,
+        unsigned char thresholdValue = 128
+    );
 };
 
 }
